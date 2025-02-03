da181fd486a34b0701a01e7fb3780ea6c59e2c90
Godfrey Kwan <>
2025-02-03 09:56:20 -0800

    move google api key to setting file

--- recyclingApplication/portal/services/google_services.py
+++ recyclingApplication/portal/services/google_services.py
@@ -1,14 +1,20 @@
 import googlemaps
+from django.conf import settings
 
-
-GMAPS_API_KEY = "AIzaSyCK_dd-qY0FadiLl7idwLnxA3M9A7tgyRs"
+# Setting up the Google Maps API key. Get the 
+# key from the settings file.
+GMAPS_API_KEY = settings.GOOGLE_MAP_KEY
 gmaps = googlemaps.Client(key=GMAPS_API_KEY)
 
+# Function to get coordinate form an address
 def get_coordinates(address):
+    # Initializing the Google Map Client
     geocode_result = gmaps.geocode(address)
+
+    # Extracting the Latitude and Longitude if result is return
     if geocode_result:
         location = geocode_result[0]["geometry"]["location"]
         place = f"{location['lat']},{location['lng']}"
 
         return f"https://www.google.com/maps/embed/v1/place?key={GMAPS_API_KEY}&q={place}"
-    return None
+    return None # return null if no data is found
