078b9b07116e2c1fdde50d126b2730f79f9e8fb6
Godfrey Kwan <>
2025-03-19 17:14:52 -0700

    add static files

--- recyclingApplication/recyclingApplication/settings.py
+++ recyclingApplication/recyclingApplication/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,6 +75,9 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'recyclingApplication.wsgi.application'
+CSRF_TRUSTED_ORIGINS = [
+    "https://recyclingproject-production.up.railway.app"
+]
 
 
 # Database
@@ -123,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") 
 
 # Tell Django where the new root static files are
 STATICFILES_DIRS = [BASE_DIR / "static"]
